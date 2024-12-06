07f292e97db5d13c6c703d0e9588fb37a87d38a3
arafatakhter505 <arafatakhterforhad@gmail.com>
2024-12-06 22:25:41 +0600

    navbar update

---
+++ utils/menus.js
@@ -0,0 +1,34 @@
+import { CiCircleInfo } from "react-icons/ci";
+import { FaCode } from "react-icons/fa";
+import { GoHome } from "react-icons/go";
+import { MdOutlineReviews, MdWorkOutline } from "react-icons/md";
+
+const menus = [
+  {
+    title: "Home",
+    url: "/",
+    icon: <GoHome className="w-6 h-6" />,
+  },
+  {
+    title: "About",
+    url: "/about",
+    icon: <CiCircleInfo className="w-6 h-6" />,
+  },
+  {
+    title: "Services",
+    url: "/services",
+    icon: <FaCode className="w-6 h-6" />,
+  },
+  {
+    title: "Career",
+    url: "/career",
+    icon: <MdWorkOutline className="w-6 h-6" />,
+  },
+  {
+    title: "Testimonial",
+    url: "/testimonial",
+    icon: <MdOutlineReviews className="w-6 h-6" />,
+  },
+];
+
+export default menus;
